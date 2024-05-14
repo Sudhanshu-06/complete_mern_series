ca418b42d0a1df9a4657dc2a1e7791f1e843029e
Sudhanshu-06 <sudhanshu767778@gmail.com>
2024-05-14 23:48:55 +0530

    push

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("Sudhanshu");
+console.log(2+2);
+
+// var a="abc";
+// var a="bacd";
+// var a=9564;
+// console.log(a);
+// --var can be redeclare and reintialize again
+// -- let can't be redeclare but you can reintialize it.
+
+let a=15;
+a="abc"
+console.log(a);
