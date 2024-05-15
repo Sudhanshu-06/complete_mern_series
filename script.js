51e336ca554323bb9590faab2a6ca3b4454bacaa
Sudhanshu-06 <sudhanshu767778@gmail.com>
2024-05-15 09:44:25 +0530

    push

--- script.js
+++ script.js
@@ -1,13 +1,61 @@
-console.log("Sudhanshu");
-console.log(2+2);
+// console.log("Sudhanshu");
+// console.log(2+2);
 
 // var a="abc";
 // var a="bacd";
 // var a=9564;
 // console.log(a);
-// --var can be redeclare and reintialize again
-// -- let can't be redeclare but you can reintialize it.
+// --var can be redeclare and reintialize again---- is a function scope
+// -- let can't be redeclare but you can reintialize it.---is a block scope
+// -- const can't be redeclare and also can't reintialize
 
-let a=15;
-a="abc"
-console.log(a);
+// let a=15;
+// a="abc"
+// console.log(a);
+
+// const a=10;
+// console.log(a);
+
+
+// -- javaScript has 8 datatypes
+// --String
+// --Number
+// --BigInt
+// --Boolean
+// --undefined
+// --null
+// --Symbol
+// --Object
+
+// --Type conversion
+// Number
+// String
+// Boolean
+
+// var a=+"123";
+// var b=a+1;
+// console.log(b);
+// console.log(typeof a);
+
+
+// var a="12"+1+2
+// var b=1+2+"12";
+// console.log(b);
+// console.log(a);
+
+// var ans=134+""
+// console.log(ans);
+// console.log(typeof ans);
+
+// var a=1123;
+// var b=String(a);
+// console.log(typeof b);
+
+// var a=123;
+// var b=a+"Hello";
+// console.log(b);
+
+// var a="Hello";
+// var b=Number(a)+1;
+// console.log(b);
+// console.log(typeof b);
