4a032b45eb66067d260a5cb9fa7ebe5935c95466
Sudhanshu-06 <sudhanshu767778@gmail.com>
2024-08-27 19:27:26 +0530

    done

---
+++ JavaScript/Introduction to Data types/main.js
@@ -0,0 +1,21 @@
+console.log("Hello")
+
+let a= 5;
+console.log(a);
+
+var b= 10;
+console.log(b);
+
+const c= 15;
+console.log(c);
+
+let productPrices=15000;
+
+let productDiscount=15;
+
+let ProductName="Apple iphone";
+
+let isAvailable=false;
+
+
+ 
