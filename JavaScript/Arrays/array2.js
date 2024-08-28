8b83fc6a951fbea381966558e85a5882d87f1979
Sudhanshu-06 <sudhanshu767778@gmail.com>
2024-08-28 19:10:09 +0530

    done

---
+++ JavaScript/Arrays/array2.js
@@ -0,0 +1,9 @@
+let arr=[1,2,3,4,5,6,7,8,9];
+console.log(arr.length);     // length is not a function it is a property
+arr.push(10);
+console.log(arr);
+arr.pop(); 
+console.log(arr);
+arr.reverse();
+console.log(arr);
+console.log(arr.indexOf(6));
---
+++ JavaScript/Arrays/arrays1.js
@@ -0,0 +1,12 @@
+let arr1=[];    //  empty arrya 
+let arr2=["Sudh", true,10,-1,null,"singh"]; 
+console.log(arr1, arr2);
+
+console.log(arr2[3]);
+
+//we can update also
+arr2[3]=100;
+console.log(arr2);
+
+arr2[10]="js"
+console.log(arr2);
