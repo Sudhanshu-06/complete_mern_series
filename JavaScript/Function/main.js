3c2cb836a75dfff869596a7695df77d777bcbfb6
Sudhanshu-06 <sudhanshu767778@gmail.com>
2024-08-28 12:40:08 +0530

    done

---
+++ JavaScript/Function/main.js
@@ -0,0 +1,26 @@
+// function greetings(){
+//     console.log("my logic 1");
+//     console.log("my logic 2");
+//     console.log("my logic 3");
+// }
+// greetings();
+// // greetings();
+// // greetings();
+
+// console.log(Math.sqrt(100));
+
+function isEvenOrOdd(x){
+    if(x%2==0){
+        console.log("Even");
+    }else{
+        console.log("Odd");
+    }
+}
+isEvenOrOdd(97); 
+
+
+function multiply(a,b){
+    console.log(a*b);
+}
+multiply(10,2);
+
