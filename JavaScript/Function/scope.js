f711aa029bb3962a289c70e6205bb4cc8ef71057
Sudhanshu-06 <sudhanshu767778@gmail.com>
2024-08-31 13:10:26 +0530

    done

--- JavaScript/Function/scope.js
+++ JavaScript/Function/scope.js
@@ -1,16 +1,28 @@
 //Global scope
-let x=10;
-function fun(){
-    console.log(x);
-}
-fun()
-console.log(x);
+// let x=10;
+// function fun(){
+//     console.log(x);
+// }
+// fun()
+// console.log(x);
 // local scope
-function fun(){
-    let x=10;
-    console.log(x);
-}
-fun();
-console.log("value of x is",x);
+// function fun(){
+//     let x=10;
+//     console.log(x);
+// }
+// fun();
+// console.log("value of x is",x);
 
 // function scope
+
+var teacher="Sudhanshu"
+function fun(){
+    var teacher="Rajat"
+    console.log("hello",teacher)
+}
+function gun(){
+    var student="Aman";
+    console.log(student);
+}
+fun();
+gun();
