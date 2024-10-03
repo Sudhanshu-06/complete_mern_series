7080080ddd6378a6562d4155a8e84fa893369715
Sudhanshu-06 <sudhanshu767778@gmail.com>
2024-10-03 23:56:34 +0530

    basic done

---
+++ Javascript_with_Harshit/array_and_object/file1.js
@@ -0,0 +1,13 @@
+// let fruit= ["apple", "orange", "Lichi"]
+// console.log(fruit);
+// console.log(fruit[1]);
+
+// let mixed= ["apple", "orange", 1,2,3,null];
+// console.log(mixed[4]);
+
+let fruit= ["apple", "orange", "Lichi"]
+fruit[1]="grapes"
+console.log(fruit);
+
+let name=["sudhabsu","ttrohan"]
+console.log(name);
